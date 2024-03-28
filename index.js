6cdbb705bffc7e003da6665570843a02b083cece
Joy <joynandako1@gmail.com>
2024-03-28 15:10:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [bessie, ,dolly, babe, little] =farmAnimals.split(' ');
+const [blackAndWhite, ,black, pink, ,] =farmAnimals.split(' ');
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -24,10 +30,26 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { muppetName, color, song, job, partner } = muppet;
+
+const { 
+  album: { 
+    theMuppetMovie: { 
+      song2, 
+      song4 
+    } 
+  }, 
+  nestedJob, 
+  nestedPartner 
+} = nestedMuppet;
+
+
+
 
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
